b4eacc5f5e6333703ed1f84abdb229511fd5d056
Elliot <12ES0222@amal.se>
2023-12-04 09:36:45 +0100

    Added PrintTodos

--- TodoList/Todolist.cs
+++ TodoList/Todolist.cs
@@ -1,5 +1,12 @@
 ﻿class Todolist {
 
     List<string> Todos = new List<string>();
-
-}
+    
+    public void PrintTodos()
+    {
+       for (int i = 0; i < Todos.Count; i++)
+        {
+            Console.WriteLine("Task: " + Todos[i]);
+        }
+    }
+} 
